14087e99920ad7ca78263f989c3f16e010893862
Dongxu.Wang <wangdongxuking61@gmail.com>
2017-08-25 17:20:34 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -17,6 +17,7 @@
 #include <cstdlib>
 #include <cstdint>
 #include <cmath>
+
 using namespace std;
 
 // 替换
@@ -34,20 +35,22 @@ typedef unsigned int uint;
 #define PRINT_4VAR(v0, v1, v2, v3)  printf(ss(v0)"\t" ss(v1)"\t" ss(v2)"\t" ss(v3)"\n",v0,v1,v2,v3)
 #define PRINT_VECTOR(v0)            print_vector(#v0, v0)
 #define PRINT_ARRAY(v0, n)          print_array(#v0, v0, n)
+
 template<class T>
 void print_vector(const string &name, vector<T> &vec)
 {
     cout << name << ": ";
     for (int i = 0; i < vec.size(); ++i)
-        cout << i << "(" << vec[i] << ")\t";
+        cout << vec[i] << " ";
     cout << endl;
 }
+
 template<class T>
 void print_array(const string &name, T arr[], int n)
 {
     cout << name << ": ";
     for (int i = 0; i < n; ++i)
-        cout << i << "(" << arr[i] << ")\t";
+        cout << arr[i] << " ";
     cout << endl;
 }
 
@@ -58,6 +61,7 @@ struct cmp_less
     bool operator()(T &a, T &b)
     { return a < b; }
 };
+
 template<class T>
 struct cmp_more
 {
@@ -70,10 +74,9 @@ struct cmp_more
 // new出来的变量 debug: (int(*)[10])a
 // new出来的变量访问时，下标一定不要越界!!!
 // new出来的一定要delete!!!
-// 整形容易正负溢出!!!
-// cin和cout速度不如scanf和printf, long long类型的输出是%ld
+// 整形容易正负溢出!!!(long long类型的输出是%ld，否则出错)
+// cin和cout速度不如scanf和printf,可使用sync_with_stdio优化
 ///////////////////////////////////////////////////////////////////////////////////////
-
 
 int main()
 {
@@ -81,7 +84,7 @@ int main()
     //freopen("/Users/xuxu/Desktop/input.in", "r", stdin);
     //freopen("/Users/xuxu/Desktop/output.out", "w", stdout);
     int n;
-    scanf("%d", &n);
-    printf("%d\n", n);
+    cin >> n;
+    cout << n << endl;
     return 0;
 }
